83f4ff15f16fec1e03ddf1533bbe7ed4acbc8ff6
Istiak60 <istisquare@gmail.com>
2022-01-18 11:20:12 +0600

    Product table

---
+++ database/migrations/2022_01_18_045004_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_title');
+            $table->string('product_image');
+            $table->double('rating');
+            $table->double('price');
+            $table->text('short_description');
+            $table->text('description');
+            $table->string('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
